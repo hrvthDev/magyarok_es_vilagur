39884c82793768331318351a22052a1878af7451
hrvth-dev <hrvth.dev@gmail.com>
2025-09-09 17:13:56 +0200

    Modosítás, validálás.

--- Backend/controller/authController.js
+++ Backend/controller/authController.js
@@ -1,54 +1,67 @@
 const db = require("../db/database.js");
+const bcrypt = require("bcrypt");
 
+const registerForm = async (req, res) => {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const check = "SELECT * FROM users WHERE username = ?";
+    const insert = "INSERT INTO users (username, password) VALUES (?, ?)";
 
-const registerForm =  async (req, res) => {
+    try {
+        db.query(check, [username], (err, results) => {
+            if (err) {
+                console.error("Hiba a lekérdezésnél:", err);
+                return res
+                    .status(500)
+                    .json({ message: "Adatbázis hiba történt!" });
+            }
 
-const {username, password} = req.body;
+            if (results.length > 0) {
+                return res.status(400).json({
+                    message: "Ez a felhasználónév már foglalt!",
+                });
+            }
 
+            db.query(insert, [username, hashedPassword], (err2) => {
+                if (err2) {
+                    console.error("Hiba a beszúrásnál:", err2);
+                    return res
+                        .status(500)
+                        .json({ message: "Szerver hiba történt!" });
+                }
 
-  try{
-
-    const insert = "INSERT INTO users (username, password) VALUES (?,  ?)";
-
-    db.query(insert, [username, password], (err) => {
-
-      if(err){
-        return res.status(500).json({message: "Szerver hiba történt!"});
-      }else{
-        res.status(201).json({message: "Sikeresen regisztráltál!"});
-      }
-    })
-  
-  }
-
-  catch(err){
-    console.log("Szerver hiba történt!", err);
-    res.status(500).json({message: "Szerver hiba történt!"});
-  }
-
-}
-
-
-const loginForm = (req, res) => {
-  const { username, password } = req.body;
-
-  const select = "SELECT * FROM users WHERE username = ? AND password = ?";
-
-  db.query(select, [username, password], (err, results) => {
-    if (err) {
-      console.error("MySQL hiba:", err);
-      return res.status(500).json({ message: "Adatbázis hiba történt!" });
+                return res
+                    .status(201)
+                    .json({ message: "Sikeresen regisztráltál!" });
+            });
+        });
+    } catch (err) {
+        console.error("Szerver hiba történt:", err);
+        return res.status(500).json({ message: "Szerver hiba történt!" });
     }
-
-    if (!results || results.length === 0) {
-      return res.status(400).json({ message: "Hibás felhasználónév vagy jelszó!" });
-    }
-
-    res.json({ message: `Szia ${results[0].username}, sikeresen bejelentkeztél!` });
-  });
 };
 
+const loginForm = (req, res) => {
+    const { username, password } = req.body;
 
+    const select = "SELECT * FROM users WHERE username = ? AND password = ?";
 
+    db.query(select, [username, password], (err, results) => {
+        if (err) {
+            console.error("MySQL hiba:", err);
+            return res.status(500).json({ message: "Adatbázis hiba történt!" });
+        }
 
-module.exports = {registerForm, loginForm};
+        if (!results || results.length === 0) {
+            return res
+                .status(400)
+                .json({ message: "Hibás felhasználónév vagy jelszó!" });
+        }
+
+        res.json({
+            message: `Szia ${results[0].username}, sikeresen bejelentkeztél!`,
+        });
+    });
+};
+
+module.exports = { registerForm, loginForm };
