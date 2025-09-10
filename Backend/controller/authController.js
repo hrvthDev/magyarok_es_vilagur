ca34d0712622f7e3a206563ff2b924137a33892a
hrvth-dev <hrvth.dev@gmail.com>
2025-09-10 20:08:04 +0200

    Jelszó hash ellenőrzési hiba javítása bejelentkezésnél.

--- Backend/controller/authController.js
+++ Backend/controller/authController.js
@@ -1,5 +1,7 @@
 const db = require("../db/database.js");
 const bcrypt = require("bcrypt");
+const util = require("util");
+
 
 const registerForm = async (req, res) => {
     const { username, password } = req.body;
@@ -41,27 +43,47 @@ const registerForm = async (req, res) => {
     }
 };
 
+
+
+
 const loginForm = (req, res) => {
-    const { username, password } = req.body;
+  const { username, password } = req.body;
+  const select = "SELECT * FROM users WHERE username = ?";
 
-    const select = "SELECT * FROM users WHERE username = ? AND password = ?";
+  db.query(select, [username], async (err, results) => {
+  if (err) {
+    console.error("Adatbázis hiba:", err);
+    return res.status(500).json({ message: "Szerver hiba történt!" });
+  }
 
-    db.query(select, [username, password], (err, results) => {
-        if (err) {
-            console.error("MySQL hiba:", err);
-            return res.status(500).json({ message: "Adatbázis hiba történt!" });
-        }
+  if (!results || results.length === 0) {
+    return res.status(400).json({ message: "Hibás felhasználónév vagy jelszó!" });
+  }
 
-        if (!results || results.length === 0) {
-            return res
-                .status(400)
-                .json({ message: "Hibás felhasználónév vagy jelszó!" });
-        }
+  const user = results[0];
 
-        res.json({
-            message: `Szia ${results[0].username}, sikeresen bejelentkeztél!`,
-        });
-    });
+  console.log("Kliens által küldött jelszó:", password);
+  console.log("Hash az adatbázisból:", user.password);
+
+  try {
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    console.log("Compare eredmény:", isPasswordValid);
+
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: "Hibás felhasználónév vagy jelszó!" });
+    }
+
+    return res.json({ message: `Szia ${user.username}, sikeresen bejelentkeztél!` });
+  } catch (e) {
+    console.error("bcrypt hiba:", e);
+    return res.status(500).json({ message: "Szerver hiba történt!" });
+  }
+});
+
+
+
+
 };
+
 
 module.exports = { registerForm, loginForm };
